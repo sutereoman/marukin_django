d92989cf3dd4ce3874df1c7221138f2773e785ff
sutereoman <sutereomandcg@gmail.com>
2022-05-28 13:11:39 +0900

    0528ジャンゴCSS分かった

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -3,6 +3,8 @@ from . import views
  
 urlpatterns = [
     path('template/', views.index_template, name='index_template'),
+    path('menu/', views.menu_template, name='menu_template'),
     path('map/', views.map_template, name='map_template'),
+    path('gallery/', views.gallery_template, name='gallery_template'),
 
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -4,5 +4,11 @@ from django.shortcuts import render
 def index_template(request):
     return render(request, 'index.html')
 
+def menu_template(request):
+    return render(request, 'menu.html')
+
 def map_template(request):
     return render(request, 'map.html')
+
+def gallery_template(request):
+    return render(request, 'gallery.html')
